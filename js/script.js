e64c85431f12db1caaa5ba583723412e26ae6430
PC <sobolinskii@mail.ru>
2025-03-18 14:33:37 +0800

    complete template

--- js/script.js
+++ js/script.js
@@ -36,4 +36,15 @@ $(document).ready(function () {
 			},
 		},
 	})
+
+	// header burger
+	$('.header__burger').click(function () {
+		$('.burger-menu__nav').toggleClass('hide')
+	})
+
+	$(document).click(function (e) {
+		if (!$(e.target).closest('.header__burger').length) {
+			$('.burger-menu__nav').addClass('hide')
+		}
+	})
 })
